5ebb16e5b5c94985874fe17079eca245b6fcf51b
TANNEERU SAI KUMAR <87522566+saikumar5901@users.noreply.github.com>
2021-07-16 14:43:58 +0530

    Add files via upload

---
+++ tc.py
@@ -0,0 +1,66 @@
+#string conversion
+a = '10'
+print(type(a), a)
+b = int(a)
+print(type(b), b)
+
+a = '10'
+print(type(a), a)
+b = float(a)
+print(type(b), b)
+
+a = '10'
+print(type(a), a)
+b = bool(a)
+print(type(b), b)
+
+#Integer Conversion
+
+a = 20
+print(type(a), a)
+b = str(a)
+print(type(b), b)
+
+a = 20
+print(type(a), a)
+b = float(a)
+print(type(b), b)
+
+a = 20
+print(type(a), a)
+b = bool(a)
+print(type(b), b)
+
+#Float Conversion
+
+a = 30.0
+print(type(a), a)
+b = str(a)
+print(type(b), b)
+
+a = 30.0
+print(type(a), a)
+b = int(a)
+print(type(b), b)
+
+a = 30.0
+print(type(a), a)
+b = bool(a)
+print(type(b), b)
+
+#Boolean Conversion
+
+a = True
+print(type(a), a)
+b = str(a)
+print(type(b), b)
+
+a = True
+print(type(a), a)
+b = int(a)
+print(type(b), b)
+
+a = True
+print(type(a), a)
+b = float(a)
+print(type(b), b)
